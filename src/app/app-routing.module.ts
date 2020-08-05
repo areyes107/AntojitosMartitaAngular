79cba650af9edf1e96094ebd7d4629edebf024e3
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-05 14:33:10 -0600

    app/app-routing.module: added combos and saucers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CombosComponent } from './components/combos/combos.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { SaucersComponent } from './components/saucers/saucers.component';
 
 
 const routes: Routes = [
  {path: '', component: HomeComponent}, // se utiliza solo para la página principal, 1 vez
  {path: 'home', component: HomeComponent},
+ {path: 'saucers', component: SaucersComponent},
+ {path: 'combos', component: CombosComponent},
  {path: '**', component: NotfoundComponent},
  
 ];
