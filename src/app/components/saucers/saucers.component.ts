7235f4ca00f944cb34b1e2e97714fa9af1d7ef10
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-05 14:33:09 -0600

    app/components/saucers/saucers.component: create saucers component

---
+++ src/app/components/saucers/saucers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saucers',
+  templateUrl: './saucers.component.html',
+  styleUrls: ['./saucers.component.css']
+})
+export class SaucersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
