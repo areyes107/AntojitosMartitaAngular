f72524e545a302397c2077820ef51de04ad0c05b
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-05 14:33:06 -0600

    app/components/combos/combos.component: createcombos component

---
+++ src/app/components/combos/combos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-combos',
+  templateUrl: './combos.component.html',
+  styleUrls: ['./combos.component.css']
+})
+export class CombosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
