13707a9abf8dbb5856b5cf30df9a57f760a73c25
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-07 11:50:26 -0600

    app/services/saucer/saucer.service: created service

---
+++ src/app/services/saucer/saucer.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaucerService {
+  endpoint='http://localhost:3800/food';
+  httOptions ={
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  private extractData (res: Response){
+    let body = res;
+    return body || [] || {};
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getSaucers():Observable<any>{
+    return this.http.get(this.endpoint + 'findAll', this.httOptions).pipe(map(this.extractData));
+  }
+
+  getSaucer(search){
+    var p = {search: search};
+    var params = JSON.stringify(p);
+
+    let httOptionsAuth = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      })
+    }
+    return this.http.post(this.endpoint + 'getSaucer', params, httOptionsAuth).pipe(map(this.extractData));
+  }
+}
