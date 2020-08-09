f1745ce84e8dc59219ad1bf76e0bb99ce9acddcc
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-08 18:33:21 -0600

    app/components/combos/combos.component: created the ts component

--- src/app/components/combos/combos.component.ts
+++ src/app/components/combos/combos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ComboService } from '../../services/combo/combo.service';
 
 @Component({
   selector: 'app-combos',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./combos.component.css']
 })
 export class CombosComponent implements OnInit {
+  combos = [];
+  search;
 
-  constructor() { }
+  constructor(private restCombo: ComboService) { }
 
   ngOnInit(): void {
+    this.getCombos();
+  }
+
+  getCombos(){
+    this.restCombo.getCombos().subscribe(res=>{
+      console.log(res);
+      this.combos = res.comboFound;
+      console.log(this.combos);
+    })
+  }
+
+  onSubmit(){
+
   }
 
 }
