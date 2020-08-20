626fc1e3c8fe0ab2a4457bfd775b7bf6a3890fd4
areyes107 <angelmrg2000@hotmail.com>
2020-08-19 22:43:48 -0600

    app/components/login/login.component: created new component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loading = false;
+  loginForm: FormGroup;
+  constructor(private firebaseAuth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  async handleSubmit() {
+    this.loading = true;
+    await this.firebaseAuth.signInWithEmailAndPassword(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    );
+    console.log(await this.firebaseAuth.currentUser)
+    this.loading = false;
+  }
+}
