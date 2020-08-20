6aeab0bc5499fdcc5a1d850ce58b130369f161ee
areyes107 <angelmrg2000@hotmail.com>
2020-08-19 22:43:49 -0600

    app/app.module: added new routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment.prod';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CombosComponent } from './components/combos/combos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { SaucersComponent } from './components/saucers/saucers.component';
-import { CombosComponent } from './components/combos/combos.component';
 import { SearchPipe } from './pipes/search.pipe';
-import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,9 @@ import { environment } from '../environments/environment.prod';
     SaucersComponent,
     CombosComponent,
     SearchPipe,
-    AcercaDeComponent
+    AcercaDeComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +39,9 @@ import { environment } from '../environments/environment.prod';
     FormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    ReactiveFormsModule
   ],
   providers: [
 
