5683d4b1beb99b25cfce3800d6dc132aa34dee88
areyes107 <angelmrg2000@hotmail.com>
2020-08-19 22:43:49 -0600

    app/app-routing.module: added new imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { CombosComponent } from './components/combos/combos.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { SaucersComponent } from './components/saucers/saucers.component';
 
@@ -10,6 +12,8 @@ import { SaucersComponent } from './components/saucers/saucers.component';
 const routes: Routes = [
  {path: '', component: HomeComponent}, // se utiliza solo para la página principal, 1 vez
  {path: 'home', component: HomeComponent},
+ {path: 'login', component: LoginComponent},
+ {path: 'admin', component: AdminComponent},
  {path: 'saucers', component: SaucersComponent},
  {path: 'combos', component: CombosComponent},
  {path: 'acercaDe', component: AcercaDeComponent},
