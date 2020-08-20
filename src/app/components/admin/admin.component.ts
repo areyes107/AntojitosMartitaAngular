443fc35fb2b79fe359e329c07f8d0608d99d995a
areyes107 <angelmrg2000@hotmail.com>
2020-08-19 22:43:46 -0600

    app/components/admin/admin.component: created new component

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+})
+export class AdminComponent implements OnInit {
+  loading = true;
+  constructor(private firebaseAuth: AngularFireAuth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkUser();
+  }
+  async checkUser(){
+    console.log(await this.firebaseAuth.currentUser)
+    if (! await this.firebaseAuth.currentUser) {
+      this.router.navigate(['/login']);
+    }
+    this.loading = false;
+  }
+}
