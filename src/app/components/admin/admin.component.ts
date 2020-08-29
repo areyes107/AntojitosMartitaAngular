16c3916159406f9741ea4c0dd403d986ee9c5830
areyes107 <angelmrg2000@hotmail.com>
2020-08-28 21:38:58 -0600

    app/components/admin/admin.component: html created

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,16 +10,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
-  loading = true;
-  constructor(private firebaseAuth: AngularFireAuth, private router: Router) {}
+  file: any;
+  typeOfSubmit = 'saucers';
+  toSubmitForm: FormGroup;
+  loading = false;
+  constructor(
+    private router: Router,
+    private angularFireStore: AngularFirestore,
+    private angularFireStorage: AngularFireStorage
+  ) {}
 
   ngOnInit(): void {
     this.checkUser();
+    this.buildForm();
   }
   async checkUser() {
-    console.log(await this.firebaseAuth.currentUser);
-    if (!(await this.firebaseAuth.currentUser)) {
+    if (!localStorage.getItem('user')) {
       this.router.navigate(['/login']);
+    }
+    this.loading = false;
+  }
+
+  buildForm() {
+    this.toSubmitForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      ingredients: new FormControl('', [Validators.required]),
+      price: new FormControl(0, [Validators.required]),
+    });
+  }
+
+  handdleFileChange(event) {
+    if (event.target.files.length > 0) {
+      this.file = event.target.files[0];
+      console.log(this.file);
+    }
+  }
+
+  async submit() {
+    if (this.file && this.typeOfSubmit) {
+      this.loading = true;
+      const documentCreated = await this.angularFireStore
+        .collection(this.typeOfSubmit)
+        .add(this.toSubmitForm.value);
+      const uploadTask = await this.angularFireStorage.upload(
+        `${this.typeOfSubmit}/${documentCreated.id}.${this.file.name
+          .split('.')
+          .pop()}`,
+        this.file
+      );
+      await this.angularFireStore
+        .collection(this.typeOfSubmit)
+        .doc(documentCreated.id)
+        .update({ photoUrl: await uploadTask.ref.getDownloadURL() });
+      this.file = null;
+      this.toSubmitForm.reset();
     }
     this.loading = false;
   }
